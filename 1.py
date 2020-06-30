f46c7d602676cef422c60aacbe256fba758227ce
mrtsnElizabeth <58268556+mrtsnElizabeth@users.noreply.github.com>
2020-06-30 22:11:32 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -6,4 +6,4 @@ def str_to_dict(some_str):
     return {str:some_str.count(str) for str in set(some_str)}
 
 
-print('Str to dict:', str_to_dict('dataroot_university'))
+print('Str to dict:', str_to_dict('university'))
